32518d8bb0487efb276d6a2eb2f5abc16fab459d
Gustavo Wilker <gustavowilkeravelar@gmail.com>
2021-11-21 11:13:18 -0300

    testando alguns 'form' com JS
    testando função de habilitar ou desabilitar algum campo e também função de validar um campo do formulário.

--- meuscript.js
+++ meuscript.js
@@ -1,13 +1,13 @@
 function soma(){
-    var numero1 = 2;
+    var numero1 = parseInt(prompt("digite o primeiro número: "));
   
-    var numero2 = 5;
+    var numero2 = parseInt(prompt("digite o segundo número: "));
   
     var soma = 0;
   
     soma = numero1 + numero2;
   
-    return soma;
+    return alert("A soma dos dois números é: "+soma);
   
   }
 
@@ -32,5 +32,48 @@ function soma(){
   function maiuscula(texto){
 
     texto.value = texto.value.toUpperCase();
+  
+  }
+  function HabilitarCampo(opcao){
+
+    if(opcao){
+  
+      document.formulario.nome.disabled = false;
+  
+    }
+  
+    else{
+  
+      document.formulario.nome.value = "";
+  
+      document.formulario.nome.disabled = true;
+  
+    }
+  
+  }
+
+  function valida(){
+
+    if(document.formularioBusca.busca.value == ""){
+  
+      alert("Preencha o campo BUSCA corretamente");
+  
+      document. formularioBusca.busca.focus();
+  
+      return false;
+  
+    }
+  
+    if(document.formularioBusca.busca.value.length < 3){
+  
+      alert("Informe pelo menos 3 letras!");
+  
+      document.formularioBusca.busca.focus();
+  
+      return false;
+  
+    }
+  
+    return true;
   
   }
